496d46639b1e05855917619e77b4879e231bfe32
Jojo - Ekaphop <ekaphop_won@truegamestudio.com>
2024-11-17 20:13:33 +0700

    Update game.js add scale to support responsive.

--- src/game.js
+++ src/game.js
@@ -1,11 +1,16 @@
+import { GameplayStageScene } from './GameplayStageScene.js';
 import { GameStartScene } from './GameStartScene.js';
 import { MainMenuScene } from './MainMenuScene.js';
 
 const config = {
     type: Phaser.AUTO,
-    width: 540,
-    height: 960,
-    scene: [MainMenuScene, GameStartScene], // Add both scene here
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 540,
+        height: 960,
+    },
+    scene: [MainMenuScene, GameStartScene, GameplayStageScene], // Add both scene here
 };
 
 const game = new Phaser.Game(config);
